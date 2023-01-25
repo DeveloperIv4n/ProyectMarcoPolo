9489516f71f1a4cc8d87e6886433b66f3cc168cf
DeveloperIv4n <123570888+DeveloperIv4n@users.noreply.github.com>
2023-01-25 16:22:57 +0100

    First files

---
+++ Bot.py
@@ -0,0 +1,114 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import requests
+import json
+from flask import request
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+ return render_template('index.html')
+
+@app.route('/test', methods=['POST'])
+def test():
+    output = request.get_json()
+    print(output) # This is the output that was stored in the JSON within the browser
+    print(type(output))
+    result = json.loads(output) #this converts the json output to a python dictionary
+    print(result) # Printing the new dictionary
+    print(type(result))#this shows the json converted as a python dictionary
+    return result
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
+
+
+"""
+# get the API KEY here: https://developers.google.com/custom-search/v1/overview
+API_KEY = "AIzaSyCwC86WJlKn1F35-vWdni5Op_xhRQw3jOA"
+# get your Search Engine ID on your CSE control panel
+SEARCH_ENGINE_ID = "e537746f75d704af3"
+# the search query you want
+
+query = input("Que quieres saber? \n")
+# using the first page
+page = 1
+# constructing the URL
+# doc: https://developers.google.com/custom-search/v1/using_rest
+# calculating start, (page=2) => (start=11), (page=3) => (start=21)
+start = (page - 1) * 10 + 1
+url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+
+# make the API request
+data = requests.get(url).json()
+
+# get the result items
+search_items = data.get("items")
+# iterate over 10 results found
+for i, search_item in enumerate(search_items, start=1):
+    try:
+        long_description = search_item["pagemap"]["metatags"][0]["og:description"]
+    except KeyError:
+        long_description = "N/A"
+    # get the page title
+    title = search_item.get("title")
+    # page snippet
+    snippet = search_item.get("snippet")
+    # alternatively, you can get the HTML snippet (bolded keywords)
+    html_snippet = search_item.get("htmlSnippet")
+    # extract the page url
+    link = search_item.get("link")
+    # print the results
+
+    print("="*10, f"Result #{i+start-1}", "="*10)
+    print("Title:", title)
+    print("Description:", snippet)
+    print("Long description:", long_description)
+    print("URL:", link, "\n")
+    
+    output = snippet.split(".")
+    print(snippet)
+"""
+
+
+
+"""
+# model_name = "microsoft/DialoGPT-large"
+model_name = "microsoft/DialoGPT-medium"
+# model_name = "microsoft/DialoGPT-small"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+
+
+# chatting 5 times with nucleus & top-k sampling & tweaking temperature & multiple
+# sentences
+for step in range(1):
+    # take user input
+    text = input(">> You:")
+    # encode the input and add end of string token
+    input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+    # concatenate new user input with chat history (if there is)
+    bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+    # generate a bot response
+    chat_history_ids_list = model.generate(
+        bot_input_ids,
+        max_length=1000,
+        do_sample=True,
+        top_p=0.95,
+        top_k=50,
+        temperature=0.75,
+        num_return_sequences=5,
+        pad_token_id=tokenizer.eos_token_id
+    )
+    #print the outputs
+    for i in range(len(chat_history_ids_list)):
+      output = tokenizer.decode(chat_history_ids_list[i][bot_input_ids.shape[-1]:], skip_special_tokens=True)
+      print(f"DialoGPT {i}: {output}")
+    choice_index = int(input("Choose the response you want for the next input: "))
+    chat_history_ids = torch.unsqueeze(chat_history_ids_list[choice_index], dim=0)
+"""
