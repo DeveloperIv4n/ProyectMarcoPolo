a8c1376614c8b205017c2b95c8068b85725c95ae
DeveloperIv4n <123570888+DeveloperIv4n@users.noreply.github.com>
2023-01-26 14:12:08 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-	$('#action_menu_btn').click(function(){
-		$('.action_menu').toggle();
-	});
-});
+let enviar = document.querySelector("#enviar2")
+let chat = document.querySelector("#chat")
+let mensaje = document.querySelector("#mensaje2")
+let mensaje2 = document.querySelector("#mensaje")
+let borrar = document.querySelector("#borrar2")
+
+
+enviar.addEventListener("click",()=> {
+    chat.innerHTML+=`<div class="d-flex justify-content-end mb-4">
+    <div class="msg_cotainer_send">
+        ${mensaje.value}
+        <span class="msg_time_send">8:55 AM, Today</span>
+    </div>
+    <div class="img_cont_msg">
+        <img src="https://avatars.hsoubcdn.com/ed57f9e6329993084a436b89498b6088?s=256"
+            class="rounded-circle user_img_msg">
+    </div>
+</div>`;
+mensaje2.reset()
+})
+borrar.addEventListener("click",()=> {
+    chat.innerHTML = "";
+})
